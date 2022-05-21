5bfaac4b45480bd74f5d1b821a7f8ffe61583d3d
Joshuan <joshuan.chel@gmail.com>
2022-05-21 17:16:47 +0300

    Update defaults

--- src/config/index.js
+++ src/config/index.js
@@ -1,17 +1,13 @@
 export const DEFAULT_CURRENCIES = [
     'USD',
     'EUR',
-    'RUB',
     'AED',
-    'AUD',
-    'UAH',
     'GBP',
     'BTC',
 ];
 
 export const DEFAULT_RATIOS = [
     1,
-    3,
     6,
     12,
 ];
