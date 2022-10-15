4bdb65364bf8ca37536473947ef612ca1fd3446d
Joshuan <joshuan.chel@gmail.com>
2022-10-15 18:05:17 +0300

    Add text design

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 
 import './Header.css';
+import { Text } from '@gravity-ui/uikit';
 
 export function Header({ date }) {
     return (
         <div className="Header">
             {date !== null && (
-                <div className="Header__Date">{(new Date(date)).toLocaleString()}</div>
+                <div className="Header__Date">
+                    <Text variant="code-2">{(new Date(date)).toLocaleString()}</Text>
+                </div>
             )}
-            <div className="Header__Title">Currency calculator</div>
+            <div>
+                <Text variant="display-2">Currency calculator</Text>
+            </div>
         </div>
     );
 }
