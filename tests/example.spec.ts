17e73ac4d82c767c57317490b7b4b142f01cf788
Joshuan <joshuan.chel@gmail.com>
2022-11-27 16:41:30 +0100

    test: Add test with data.json request

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,10 +1,20 @@
 import { test, expect } from '@playwright/test';
 
 test('homepage has title', async ({ page }) => {
+	await page.route('**/data.json*', route => {
+		return route.fulfill({
+			status: 200,
+			contentType: 'application/octet-stream',
+			body: JSON.stringify(require('./data.mock.json')),
+		});
+	});
+
   await page.goto('/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Money exchanges/);
+  await expect(await page.title()).toEqual("Money exchanges");
+	// await page.waitForTimeout(1000);
+	await expect(page.locator('.yc-text_variant_code-2')).toHaveText("11/27/2022, 11:15:03 AM");
 
   // // create a locator
   // const getStarted = page.getByRole('link', { name: 'Get started' });
